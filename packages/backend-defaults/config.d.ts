8ee3acb37b636597fa291d5cdcd85cf4bea350e7
Hellgren Heikki <heikki.hellgren@op.fi>
2025-02-04 08:06:37 +0200

    feat: support postgres store for rate limiting
    Signed-off-by: Hellgren Heikki <heikki.hellgren@op.fi>

--- packages/backend-defaults/config.d.ts
+++ packages/backend-defaults/config.d.ts
@@ -721,6 +721,22 @@ export interface Config {
                 connection: string;
               }
             | {
+                client: 'postgres';
+                connection:
+                  | string
+                  | {
+                      /**
+                       * @visibility secret
+                       */
+                      password?: string;
+                      /**
+                       * Other connection settings
+                       * @see https://node-postgres.com/apis/client
+                       */
+                      [key: string]: unknown;
+                    };
+              }
+            | {
                 client: 'memory';
               };
           /**
